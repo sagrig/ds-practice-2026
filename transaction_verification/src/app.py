4b5620d6dde7a2c8814aa0ce7aae4bed70404a6b
Sahak Grigoryan <sahakgrigoryan.am@gmail.com>
2026-03-02 19:10:13 +0200

    Week 2: add transaction_verification

---
+++ transaction_verification/src/app.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import grpc
+from concurrent import futures
+
+FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
+grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/transaction_verification'))
+sys.path.insert(0, grpc_path)
+
+import transaction_verification_pb2 as tv_pb2
+import transaction_verification_pb2_grpc as tv_pb2_grpc
+
+
+class TransactionService(tv_pb2_grpc.TransactionServiceServicer):
+
+    def VerifyTransaction(self, request, context):
+        response = tv_pb2.TransactionResponse()
+
+        if request.items:
+            response.valid = True
+        else:
+            response.valid = False
+
+        return response
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor())
+    tv_pb2_grpc.add_TransactionServiceServicer_to_server(
+        TransactionService(), server
+    )
+
+    server.add_insecure_port("[::]:50052")
+    server.start()
+    print("Transaction Verification running on 50052")
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
